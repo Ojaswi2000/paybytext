5a36206571c3bf4041838f8875b0f2b6a67f022d
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-04-11 22:11:46 +0530

    changed

--- app.js
+++ app.js
@@ -165,15 +165,15 @@ app.get('/api/v1/paybytext/list/byIdentity/:id/',
 
 //DELETE request for a single id
 app.get('/api/v1/paybytext/:id/',
-    body('_id')
-        .isAlphanumeric()
-        .exists(),
-    body('accountNumber')
-        .isLength({min:5})
-        .exists(),
-    body('accountName')
-        .exists()
-        .isString(),
+    // body('_id')
+    //     .isAlphanumeric()
+    //     .exists(),
+    // body('accountNumber')
+    //     .isLength({min:5})
+    //     .exists(),
+    // body('accountName')
+    //     .exists()
+    //     .isString(),
     handleDeletePayByTextById)
 
 
